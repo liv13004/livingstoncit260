d921c23beaba70d605c9cabcc3ac2a68d822a1bc
andrew <andrew@para-laptop>
2015-01-29 23:17:10 -0700

    initial commit

---
+++ treasure/src/byui/cit260/treasure/model/Player.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treasure.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author andrew
+ */
+public class Player implements Serializable{
+    private String name;
+    private boolean gender;
+
+    public Player() {
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", gender=" + gender + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + (this.gender ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.gender != other.gender) {
+            return false;
+        }
+        return true;
+    }
+    
+}
